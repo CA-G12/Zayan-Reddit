7a485b2f22268a9a1e254a98dd43f541b7128cdb
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-09-03 15:13:34 +0300

    Created a server #3

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const compression = require('compression');
+const { join } = require('path');
+
+const app = express();
+app.set('port', process.env.PORT || 8000);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(compression());
+app.use(express.static(join(__dirname, 'front-end/public')));
+
+module.exports = app;
---
+++ server/index.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-undef */
+const app = require('./app');
+
+const port = app.get('port');
+app.listen(port, () => console.log(`start listen at http://localhost:${port}`));

633aea7532ba288c11452fa514ac3327edfca2d3
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-09-06 13:08:19 +0300

    Added verifyJWT as util Relates #36

--- server/utils/index.js
+++ server/utils/index.js
@@ -2,5 +2,6 @@ const hashPassword = require('./hashPassword');
 const signJWT = require('./signJWT');
 const CustomizedServerErrors = require('./customizedServerErrors');
 const comparePasswords = require('./comparePasswords');
+const verifyJWT = require('./verifyJWT');
 
-module.exports = { hashPassword, signJWT, CustomizedServerErrors, comparePasswords };
+module.exports = { hashPassword, signJWT, CustomizedServerErrors, comparePasswords, verifyJWT };

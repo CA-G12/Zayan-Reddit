38509d69ac4a3941f636035f3ba9f4db7bcf6d7e
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-09-06 15:23:59 +0300

    Added logout endpoint Relates #49

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const { signup, signin } = require('../controllers');
+const { signup, signin, logout } = require('../controllers');
 
 router.post('/signup', signup);
 router.post('/signin', signin);
-
+router.get('/logout', logout);
 
 module.exports = router;

a766bfea886c7069f4a692c6e7ca81b3def63dce
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-09-07 18:56:22 +0300

    Added controller for adding a post to the db Relates #41

--- server/controllers/posts/addPost.js
+++ server/controllers/posts/addPost.js
@@ -1,5 +1,27 @@
-const addPost = (req, res) => {
+const { postValidation } = require('../../validation');
+const { CustomizedServerErrors } = require('../../utils');
+const { addPostQuery } = require('../../database/queries');
+// post validation
+// addpost
+// send post in res
+// catch validation errors
+
+const addPost = (req, res, next) => {
   console.log('in addPost router', req.body);
+  const { content, image } = req.body;
+  const { id, username, avatar } = req.token;
+  const userId = id;
+  console.log('userId', userId);
+  postValidation({ userId, content, image })
+    .then(() => addPostQuery({ content, image, userId }))
+    .then((postData) => { req.postData = postData; next(); })
+    .catch((err) => {
+      if (err.details) {
+        // console.log(err.details[0].message);
+        next(new CustomizedServerErrors(401, `Validation error : ${err.details[0].message}`));
+      }
+      next(err);
+    });
 };
 
 module.exports = addPost;

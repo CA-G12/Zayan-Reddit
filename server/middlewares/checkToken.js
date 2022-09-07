c0790ea62fd1fbe179eaa8c4135ec42af2a603af
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-09-07 18:54:14 +0300

    Stop ending the req-res cycle after checking the token and let it go the next middleware Relates #41

--- server/middlewares/checkToken.js
+++ server/middlewares/checkToken.js
@@ -1,17 +1,19 @@
-const { CustomizedServerErrors, verifyJWT } = require('../utils');
+const { verifyJWT } = require('../utils');
 
 const checkToken = (req, res, next) => {
   console.log('in checkToken');
   console.log(req.cookies.token);
   if (!req.cookies.token) {
     console.log('no token');
-    res.json({ istoken: false });
+    res.redirect('/');
+    // res.json({ istoken: false });
   } else {
     const { token } = req.cookies;
     verifyJWT(token)
       .then((decoded) => {
         req.token = decoded;
-        res.json({ istoken: true, username: req.token.username, avatar: req.token.avatar });
+        next();
+        // res.json({ istoken: true, username: req.token.username, avatar: req.token.avatar });
       })
       .catch((err) => next(err));
   }

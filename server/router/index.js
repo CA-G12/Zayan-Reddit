2e518dcdbdbe7f59c80b033bd53724b596c9c4ae
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-09-07 18:51:18 +0300

    Created /createPost and /addPost endpoints and connected them to the proper middlewares Relates #41

--- server/router/index.js
+++ server/router/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
-const { checkToken } = require('../middlewares');
+const { checkToken, sendUserData } = require('../middlewares');
+const {createPostPage} = require('../controllers')
 const AuthRoutes = require('./auth');
 const postsRoutes = require('./posts');
 
-router.get('/main', checkToken);
+router.get('/main', checkToken, sendUserData);
 router.use(AuthRoutes);
-router.use(postsRoutes);
+router.use(checkToken, postsRoutes);
 
 module.exports = router;

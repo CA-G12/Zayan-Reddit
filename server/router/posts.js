b5b97aba6016c7fcbc8ecee1f65670df17e30f83
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-09-08 03:15:32 +0300

    Added needed enpoints related to posts Relates #41 #57 #58

--- server/router/posts.js
+++ server/router/posts.js
@@ -1,9 +1,14 @@
 const router = require('express').Router();
-const { addPost, viewPost } = require('../controllers');
+const {
+  addPost, viewPost, getPostData, getAllPosts,
+} = require('../controllers');
 const { checkToken } = require('../middlewares');
 const { createPostPage } = require('../controllers');
 
 router.get('/createPost', checkToken, createPostPage);
-router.post('/addPost', addPost, viewPost);
+router.post('/addPost', checkToken, addPost);
+router.get('/post/:id/display', viewPost);
+router.get('/post/:id/data', getPostData);
+router.get('/posts', getAllPosts);
 
 module.exports = router;

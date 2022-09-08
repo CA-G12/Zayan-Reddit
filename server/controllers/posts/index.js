4c6fc2e9f0923c2ed330b27369a2a2dfdfd7522d
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-09-08 03:25:57 +0300

    Exported new controllers #57 #58

--- server/controllers/posts/index.js
+++ server/controllers/posts/index.js
@@ -1,6 +1,9 @@
 const addPost = require('./addPost');
 const viewPost = require('./viewPost');
-const createPostPage = require('./createPostPage')
+const createPostPage = require('./createPostPage');
+const getPostData = require('./getPostData');
+const getAllPosts = require('./getAllposts');
 
-
-module.exports = {addPost, viewPost, createPostPage};
+module.exports = {
+  addPost, viewPost, createPostPage, getPostData, getAllPosts,
+};

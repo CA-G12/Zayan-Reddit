54e2939c21f7e1f1da97a2076180f31e699137e6
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-09-09 03:17:03 +0300

    Created new endpoint to get all comments on specific post Relates #45

--- server/router/index.js
+++ server/router/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const { checkToken, sendUserData } = require('../middlewares');
 const AuthRoutes = require('./auth');
 const postsRoutes = require('./posts');
+const commentsRoutes = require('./comments');
 
 router.get('/isLogged', checkToken, sendUserData);
 router.use(AuthRoutes);
 router.use(postsRoutes);
+router.use(commentsRoutes);
 
 module.exports = router;

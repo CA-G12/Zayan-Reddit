467ce893615bf74f1a1186d970ba37ee3c3fc6f0
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-09-09 03:18:55 +0300

    Deleted unwanted comments

--- server/middlewares/checkToken.js
+++ server/middlewares/checkToken.js
@@ -4,15 +4,12 @@ const checkToken = (req, res, next) => {
   if (!req.cookies.token) {
     console.log('no token');
     res.json({ istoken: false });
-    // res.redirect('/');
-    // res.json({ istoken: false });
   } else {
     const { token } = req.cookies;
     verifyJWT(token)
       .then((decoded) => {
         req.token = decoded;
         next();
-        // res.json({ istoken: true, username: req.token.username, avatar: req.token.avatar });
       })
       .catch((err) => next(err));
   }

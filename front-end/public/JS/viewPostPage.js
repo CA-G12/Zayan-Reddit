65c4d1eb44ee55d24b4c2ed9d3b48f924aa70fff
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-09-09 03:28:19 +0300

    Handled adding comment on a post and displaying post's comments in post page Relates #45

--- front-end/public/JS/viewPostPage.js
+++ front-end/public/JS/viewPostPage.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable prefer-destructuring */
 const avatar = document.querySelector('.avatar');
 const addedBy = document.querySelector('.postUsername');
 const timeAdded = document.querySelector('.timeAdded');
@@ -5,10 +7,10 @@ const content = document.querySelector('.content');
 const image = document.querySelector('.image');
 const addCommentBtn = document.querySelector('.addCommentBtn');
 const CommentInput = document.querySelector('#CommentInput');
+const commentsSection = document.querySelector('.commentsSection');
 
 const postId = window.location.href.split('/')[4];
 console.log(postId);
-// eslint-disable-next-line no-use-before-define
 fetch(`/post/${postId}/data`).then((res) => res.json()).then((postData) => handleSinglePostDataDOM(postData)).catch(console.log);
 
 const handleSinglePostDataDOM = (postDataObj) => {
@@ -21,13 +23,25 @@ const handleSinglePostDataDOM = (postDataObj) => {
   addedBy.addEventListener('click', () => console.log(addedBy.getAttribute('userId')));
   avatar.addEventListener('click', () => console.log(avatar.getAttribute('userId')));
   console.log(avatar.getAttribute('userId'));
-  timeAdded.textContent = postDataObj.timeadded;
+  timeAdded.textContent = new Date(postDataObj.timeadded).toString().split('GMT')[0];
   content.textContent = postDataObj.content;
   if (postDataObj.image) {
     image.src = postDataObj.image;
   } else {
     image.remove();
   }
+  fetch(`/post/${postId}/comments`)
+    .then((res) => res.json())
+    .then((commentsArray) => {
+      commentsArray.forEach((comment) => renderComment({
+        commentContent: comment.content,
+        commentTimeAdded: comment.timeadded,
+        commentAvatar: comment.avatar,
+        commentUsername: comment.username,
+        commentUserId: comment.userid,
+      }));
+    })
+    .catch(console.log);
 };
 fetch('/isLogged')
   .then((res) => res.json())
@@ -37,16 +51,73 @@ fetch('/isLogged')
       console.log('no token and in comment');
       addCommentBtn.style.display = 'none';
       CommentInput.style.display = 'none';
+    } else {
+      // eslint-disable-next-line no-shadow
+      const { avatar, username } = res;
+      addCommentBtn.addEventListener('click', () => {
+        if (!CommentInput.value) {
+          // eslint-disable-next-line no-undef
+          Swal.fire({
+            title: 'Attention!',
+            text: 'There is no comment to add, please add your comment in field',
+            icon: 'warning',
+            confirmButtonText: 'OK',
+          });
+        } else {
+          fetch('/addComment', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              content: CommentInput.value,
+              userId: res.id,
+              postId,
+            }),
+          }).then((response) => response.json())
+            .then((commentData) => renderComment({
+              commentId: commentData.id,
+              commentContent: commentData.content,
+              commentTimeAdded: commentData.timeadded,
+              commentAvatar: avatar,
+              commentUsername: username,
+              commentUserId: commentData.userid,
+            }));
+          CommentInput.value = '';
+        }
+      });
     }
   })
   .catch(console.error);
-addCommentBtn.addEventListener('click', () => {
-  if (!CommentInput.value) {
-    Swal.fire({
-      title: 'Attention!',
-      text: 'There is no comment to add, please add your comment in field',
-      icon: 'warning',
-      confirmButtonText: 'OK',
-    });
-  }
-});
+
+const renderComment = (commentDataObj) => {
+  console.log('in render comment', commentDataObj);
+  const comment = document.createElement('div');
+  comment.className = 'comment';
+  const commentedBy = document.createElement('div');
+  commentedBy.className = 'commentedBy';
+  const commentUserInfo = document.createElement('div');
+  commentUserInfo.className = 'commentUserInfo';
+  const commentAvatar = document.createElement('img');
+  commentAvatar.className = 'commentAvatar';
+  commentAvatar.src = commentDataObj.commentAvatar;
+  const commentUsername = document.createElement('h4');
+  commentUsername.className = 'commentUsername';
+  commentUsername.textContent = commentDataObj.commentUsername;
+  const commentTime = document.createElement('h5');
+  commentTime.className = 'commentTime';
+  commentTime.textContent = (new Date(commentDataObj.commentTimeAdded)).toString().split('GMT')[0];
+  const commentContent = document.createElement('p');
+  commentContent.className = 'commentContent';
+  commentContent.textContent = commentDataObj.commentContent;
+  commentUserInfo.appendChild(commentAvatar);
+  commentUserInfo.appendChild(commentUsername);
+  commentedBy.appendChild(commentUserInfo);
+  commentUserInfo.setAttribute('userId', commentDataObj.commentUserId);
+  commentUserInfo.addEventListener('click', () => console.log(commentUserInfo.getAttribute('userId')));
+  commentedBy.appendChild(commentTime);
+  comment.appendChild(commentedBy);
+  comment.appendChild(commentContent);
+  const theFirstChild = commentsSection.firstChild;
+  commentsSection.insertBefore(comment, theFirstChild);
+};

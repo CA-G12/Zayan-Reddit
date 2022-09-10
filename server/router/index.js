058cb76841c8d58353d3672aa73937734eb0485e
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-09-10 21:33:51 +0300

    Created a route to get user data to fix viewing profile page when there is no posts Relates #63

--- server/router/index.js
+++ server/router/index.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 const { checkToken, sendUserData } = require('../middlewares');
+const { getUserData } = require('../controllers');
 const AuthRoutes = require('./auth');
 const postsRoutes = require('./posts');
 const commentsRoutes = require('./comments');
 
-router.get('/isLogged', checkToken, sendUserData);
 router.use(AuthRoutes);
+router.get('/isLogged', checkToken, sendUserData);
+router.get('/profileData/:id', getUserData);
 router.use(postsRoutes);
 router.use(commentsRoutes);
 

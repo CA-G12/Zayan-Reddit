7a389448f971338b232274d25ca1756c11577deb
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-09-10 15:32:26 +0300

    Removed eslint disables Relates #63

--- front-end/public/JS/main.js
+++ front-end/public/JS/main.js
@@ -1,4 +1,2 @@
-/* eslint-disable prefer-destructuring */
-/* eslint-disable no-use-before-define */
 /* eslint-disable no-undef */
 fetch('/posts').then((res) => res.json()).then((posts) => posts.reverse().forEach((postData) => renderPost(postData))).catch(console.error);
